c00a3817a866e7edf4bf82bfc8d3d4f583633a88
DyxMax <dylanattard.38320@gmail.com>
2025-08-04 15:11:16 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import time
+
+def main():
+    print("Bot lancé avec succès !")
+
+    while True:
+        print("Je suis en ligne...")
+        time.sleep(10)
+
+if __name__ == "__main__":
+    main()
